fdbbf1ef3009853b44d9b011356bd3ac11aa0787
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-11-21 15:42:37 -0300

    Criação do projeto com Spring Initializr

---
+++ src/main/java/com/gg/occurrence_manager/OccurrenceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gg.occurrence_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccurrenceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccurrenceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gg/occurrence_manager/OccurrenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.occurrence_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccurrenceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
