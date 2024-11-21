274799ab764d9a99bcd7cd6c9568fa6e3eff2d51
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-11-21 17:03:40 -0300

    Criação do CRUD de Cliente

---
+++ src/main/java/com/gg/occurrence_manager/controller/ClienteController.java
@@ -0,0 +1,40 @@
+package com.gg.occurrence_manager.controller;
+
+import com.gg.occurrence_manager.model.Cliente;
+import com.gg.occurrence_manager.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<Cliente> saveOrUpdate(@RequestBody Cliente cliente) {
+        Cliente savedCliente = clienteService.saveOrUpdate(cliente);
+        return ResponseEntity.ok(savedCliente);
+    }
+
+    @GetMapping
+    public List<Cliente> getAll() {
+        return clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+        Optional<Cliente> cliente = clienteService.findById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clienteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/gg/occurrence_manager/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.gg.occurrence_manager.service;
+
+import com.gg.occurrence_manager.model.Cliente;
+import com.gg.occurrence_manager.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente saveOrUpdate(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> findById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
