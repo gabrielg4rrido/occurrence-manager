7972e03057c8ffa416e92bb89da6cd3bb67c9c57
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-11-22 16:02:03 -0300

    Ajuste no tempo de expiração do token

--- src/main/java/com/gg/occurrence_manager/infra/security/TokenService.java
+++ src/main/java/com/gg/occurrence_manager/infra/security/TokenService.java
@@ -45,6 +45,5 @@ public class TokenService {
     }
 
     private Instant generateExpirationDate() {
-        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
-    }
+        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));    }
 }
