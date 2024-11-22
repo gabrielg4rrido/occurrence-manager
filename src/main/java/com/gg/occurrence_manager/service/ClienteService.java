85bb30e35c5f055ac4dbf00ff14eda052d25a594
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-11-22 16:32:50 -0300

    Tratando a criação de cliente

--- src/main/java/com/gg/occurrence_manager/service/ClienteService.java
+++ src/main/java/com/gg/occurrence_manager/service/ClienteService.java
@@ -1,9 +1,11 @@
 package com.gg.occurrence_manager.service;
 
+import com.gg.occurrence_manager.exception.CustomBadRequestException;
 import com.gg.occurrence_manager.model.Cliente;
 import com.gg.occurrence_manager.model.dto.ClienteDTO;
 import com.gg.occurrence_manager.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,9 +18,17 @@ public class ClienteService {
     private ClienteRepository clienteRepository;
 
     public ClienteDTO criarCliente(ClienteDTO clienteDTO) {
-        Cliente cliente = new Cliente(clienteDTO);
-        cliente = clienteRepository.save(cliente);
-        return new ClienteDTO(cliente);
+        try {
+            Cliente cliente = new Cliente(clienteDTO);
+            cliente = clienteRepository.save(cliente);
+            return new ClienteDTO(cliente);
+        } catch (DataIntegrityViolationException ex) {
+            throw new CustomBadRequestException("Erro: Dados inválidos ou em conflito. Verifique os campos enviados.");
+        } catch (IllegalArgumentException ex) {
+            throw new CustomBadRequestException("Erro: Argumento inválido fornecido.");
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro inesperado ao criar o cliente.");
+        }
     }
 
     public List<ClienteDTO> listarClientes() {
