78c509337959bd421c046ad5cc7aad9937db88bc
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-11-22 20:09:10 -0300

    Criando a configuração do MinIO

---
+++ src/main/java/com/gg/occurrence_manager/service/MinioService.java
@@ -0,0 +1,47 @@
+package com.gg.occurrence_manager.service;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.MinioException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    public MinioService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public List<String> uploadFiles(List<MultipartFile> files) {
+        List<String> paths = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+            try {
+                minioClient.putObject(PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .contentType(file.getContentType())
+                        .build());
+                paths.add(fileName);
+            } catch (MinioException | IOException | InvalidKeyException | NoSuchAlgorithmException e) {
+                throw new RuntimeException("Erro ao fazer upload para o MinIO: " + e.getMessage(), e);
+            }
+        }
+        return paths;
+    }
+}
